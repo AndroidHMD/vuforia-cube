44494ace876eb976304f6dbf291352922bf50cea
Benjamin Wiberg <benjaminwiberg@gmail.com>
2016-02-26 15:11:33 +0100

    Implement logging of camera's position (use adb to see it)

---
+++ vuforia-cube/Assets/PositionDebugger.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionDebugger : MonoBehaviour {
+	void Update () {
+		Debug.Log("Camera position: " + gameObject.transform.position);
+	}
+}
