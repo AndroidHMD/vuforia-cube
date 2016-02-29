e3a94bec180628872f54921c2e41ed84ca45d05f
Benjamin Wiberg <benjaminwiberg@gmail.com>
2016-02-29 12:53:54 +0100

    Implement networked transforms between phones in VR

---
+++ vuforia-cube/Assets/PlayerCountDebugger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCountDebugger : MonoBehaviour {
+	void Update () {
+		string str = "Connected players: ";
+
+		foreach (var player in PhotonNetwork.playerList)
+		{
+			str += "[" + player.name + ", " + player.ID + "]";
+		}
+
+		Debug.Log(str);
+	}
+}
