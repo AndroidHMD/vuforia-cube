a4ed76a5dc6f87009315564d84dcfb490dfad131
Benjamin Wiberg <benjaminwiberg@gmail.com>
2016-03-02 10:51:32 +0100

    Disable rendering/masking of tracker cube

---
+++ vuforia-cube/Assets/RemoveMaskOutBehaviourAndDisableRenderingOnStart.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveMaskOutBehaviourAndDisableRenderingOnStart : MonoBehaviour {
+	void Start () {
+		//gameObject.GetComponent<Vuforia.MaskOutBehaviour>().enabled = false;
+		//gameObject.GetComponent<MeshRenderer>().enabled = false;
+
+		Destroy(gameObject.GetComponent<Vuforia.MaskOutBehaviour>());
+		Destroy(gameObject.GetComponent<MeshRenderer>());
+	}
+}
