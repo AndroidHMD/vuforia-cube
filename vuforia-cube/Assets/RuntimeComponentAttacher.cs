8736f50d99a1096058231911694bc616872e6a88
Benjamin Wiberg <benjaminwiberg@gmail.com>
2016-03-02 11:32:30 +0100

    Create setup to remove MaskOutBehaviour from cubes + debug screen space pos of cube

---
+++ vuforia-cube/Assets/RuntimeComponentAttacher.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RuntimeComponentAttacher : MonoBehaviour {
+	public GameObject[] gameObjects;
+	public System.Type componentType;
+
+	void Start () {
+		componentType = typeof(RemoveMaskOutBehaviourAndDisableRenderingOnStart);
+		foreach(var gameObject in gameObjects)
+		{
+			//gameObject.AddComponent(component.GetType());
+			gameObject.AddComponent(componentType);
+		}
+	}
+}
---
+++ vuforia-cube/Assets/ScreenSpacePositionDebugger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenSpacePositionDebugger : MonoBehaviour {
+	public Transform monitoredObject;
+
+	void Update () {
+		debugMonitoredObjectSSPosition();
+	}
+
+	void debugMonitoredObjectSSPosition() {
+		var screenPoint = GetComponent<Camera>().WorldToScreenPoint(monitoredObject.position);
+
+		screenPoint.x /= Screen.width;
+		screenPoint.y /= Screen.height;
+
+		Debug.Log("Screen space position of cube: " + screenPoint);
+	}
+}
